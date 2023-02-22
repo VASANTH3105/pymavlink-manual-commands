65113e5ca294a5ff8072abee7b2f4e78cc04f2d3
VASANTH S <122204153+VASANTH3105@users.noreply.github.com>
2023-02-22 12:09:36 +0530

    simple commands x,y,z coordinates

---
+++ simple_commands.py
@@ -0,0 +1,63 @@
+#manual commands 	
+from pymavlink import mavutil
+import time
+import math
+from math import*
+buttons = 1 + 1 << 3 + 1 << 7
+
+
+def manual_control(x=0, y=0, z=500, yaw=0):
+
+#changes made here
+    # Convert yaw input from degrees to radians
+    yaw_rad = yaw * math.pi / 180.0
+    
+    # Calculate roll and pitch inputs based on the yaw input
+    roll = 0
+    pitch = 0
+    if yaw_rad > 0:
+        roll = 1000
+    elif yaw_rad < 0:
+        roll = -1000
+    pitch = abs(int(roll * math.tan(yaw_rad)))
+#changes end here
+
+
+
+    master.mav.manual_control_send(
+        master.target_system,
+        x,
+        y,
+        z,  # 500 means neutral throttle
+        yaw,
+        buttons
+    )
+
+
+master = mavutil.mavlink_connection('/dev/ttyACM0', baud=115200)
+print("Waiting for heartbeat...")
+master.wait_heartbeat()
+print("Heartbeat from APM (system %u component %u)" %
+      (master.target_system, master.target_system))
+master.arducopter_arm()																																																																																																																					
+print("Arming motors")
+master.motors_armed_wait()
+print("ARmed")
+time.sleep(3)
+
+for i in range(5): #3initial
+    print(i)
+    manual_control(x=300)
+    time.sleep(2)
+
+for i in range(5): #3initial
+    print(i)
+    manual_control(y=300)
+    time.sleep(2)
+    
+for i in range(3): #3initial
+    print(i)
+    manual_control(x=300)
+    time.sleep(2)
+
+
